1c949c61ed26a24236514888fadf4d8f78cf89fe
ketan <ketanexam92@gmail.com>
2024-07-11 18:26:48 +0530

    mmmmmmm

--- src/main/java/com/web/service/CityService.java
+++ src/main/java/com/web/service/CityService.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class CityService {
 
-	XX x = new XX();
-	PP p = new PP();
+	QQ q = new QQ ();
+	NN n = new NN();
 	public String cities() {
 		return "mumbai,pune";
 	}
